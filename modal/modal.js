99216bec809d0ebafc15bfca3a14325adb214fa4
usmonaliyevh <149366504+usmonaliyevh@users.noreply.github.com>
2023-10-30 16:23:42 +0500

    Add files via upload

---
+++ modal/modal.js
@@ -0,0 +1,8 @@
+let navbtn = document.querySelector('.button')
+let navbar = document.querySelector('.ul')
+
+button.onclick = () => {
+    button.classList.toggle('active'),
+    ul.classList.toggle('active')
+
+}
